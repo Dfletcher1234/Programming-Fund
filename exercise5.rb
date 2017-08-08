fc9a865930393c1ebaede3536313b1d4375ed87e
David FletcherE <dtsfletcher@gmail.com>
2017-08-08 17:42:10 -0400

    all done 6.1

--- exercise5.rb
+++ exercise5.rb
@@ -4,8 +4,7 @@ puts "Do you want to walk or run"
 user_choice = gets.chomp
 distance = 0
 
-while user_choice == "walk" || user_choice == "run"
-
+while user_choice == "walk" || user_choice == "run" || user_choice == "home"
   if user_choice == "walk"
     distance += 1
    puts "distance from home is #{distance}"
@@ -16,5 +15,13 @@ while user_choice == "walk" || user_choice == "run"
     puts "distance from hom is #{distance}"
     puts "Do you want to walk or run"
     user_choice = gets.chomp
+
+  # until user_choice == "home"
+    # if user_choice == "home"
+      puts "lazy"
+    distance = -1
+
+
+
 end
   end
