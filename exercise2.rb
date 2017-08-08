8ef6d2dc216cfa1ad6056749ed7d27dd44fac387
David FletcherE <dtsfletcher@gmail.com>
2017-08-08 14:30:56 -0400

    part 2 done

---
+++ exercise2.rb
@@ -0,0 +1,12 @@
+#How would you calculate a good tip for a 55 dollar meal? Use puts to print the answer
+puts "you should tip $#{55 * 0.20}"
+
+######
+
+puts "this is hard" + 3.to_s
+
+######
+
+puts "there are #{45628 * 7839} cans of beer in a case"
+
+puts (10 < 20 && 30 < 20) || !(10 == 11)
